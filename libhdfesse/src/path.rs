9ac26a5585ad8010bdc18af35a556d12fe13d773
Ivan Boldyrev <lispnik@gmail.com>
2021-02-26 23:54:52 +0300

    Return commented out code...

--- libhdfesse/src/path.rs
+++ libhdfesse/src/path.rs
@@ -312,8 +312,7 @@ impl<'a> Path<'a> {
 impl<'a> Display for Path<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // Well, the unwrap_or_else should never execute.
-        // f.write_str(&uri_path_to_hdfs_path(&self.path).unwrap_or_else(|_| self.path.to_string()))
-        f.write_str(&uri_path_to_hdfs_path(&self.path).unwrap())
+        f.write_str(&uri_path_to_hdfs_path(&self.path).unwrap_or_else(|_| self.path.to_string()))
     }
 }
 
